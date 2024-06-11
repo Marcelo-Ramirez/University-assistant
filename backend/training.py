c341957585f254492372ed8b1353f3f12e275f14
Marcelo Ramirez <ramirezyapura@gmail.com>
2024-06-11 09:29:55 -0400

    nueva red mejorada, tambien agregando unos itents

--- backend/training.py
+++ backend/training.py
@@ -4,9 +4,8 @@ import pickle
 import numpy as np
 
 import nltk
-from nltk.stem import WordNetLemmatizer #Para pasar las palabras a su forma raíz
+from nltk.stem import WordNetLemmatizer
 
-#Para crear la red neuronal
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
@@ -24,7 +23,7 @@ classes = []
 documents = []
 ignore_letters = ['?', '!', '¿', '.', ',']
 
-#Clasifica los patrones y las categorías
+# Clasifica los patrones y las categorías
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
@@ -39,8 +38,7 @@ words = sorted(set(words))
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
-#Pasa la información a unos y ceros según las palabras presentes en cada categoría para hacer el entrenamiento
-#Pasa la información a unos y ceros según las palabras presentes en cada categoría para hacer el entrenamiento
+# Pasa la información a unos y ceros según las palabras presentes en cada categoría para hacer el entrenamiento
 training = []
 output_empty = [0]*len(classes)
 for document in documents:
@@ -56,23 +54,26 @@ random.shuffle(training)
 training = np.array(training, dtype=object) 
 print(training) 
 
-
-#Reparte los datos para pasarlos a la red
+# Reparte los datos para pasarlos a la red
 train_x = list(training[:,0])
 train_y = list(training[:,1])
 
-#Creamos la red neuronal
+# Creamos la red neuronal con aún más neuronas y capas adicionales
 model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dense(512, input_shape=(len(train_x[0]),), activation='relu')) # Aumentamos a 512 neuronas
 model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
+model.add(Dense(256, activation='relu')) # Aumentamos a 256 neuronas
+model.add(Dropout(0.5))
+model.add(Dense(128, activation='relu')) # Añadimos una capa adicional con 128 neuronas
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu')) # Añadimos una capa adicional con 64 neuronas
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-#Creamos el optimizador y lo compilamos
+# Creamos el optimizador y lo compilamos
 sgd = SGD(learning_rate=0.001, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-#Entrenamos el modelo y lo guardamos
-train_process = model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=5, verbose=1)
+# Entrenamos el modelo y lo guardamos
+train_process = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 model.save("chatbot_model.h5", train_process)
