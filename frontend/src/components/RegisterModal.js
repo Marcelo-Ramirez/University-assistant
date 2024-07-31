7d2723208bbdeadfc0c89fdf5b68d5be9b5451c4
marcosrmc3 <mamanimarcos113@gmail.com>
2024-07-31 00:55:42 -0400

    agregando limpieza de campos username, password, carrera al momento de:  (abrir y cerrar el modal, cambio  de ventanas de registro e inicio s. y al presionar btn registro e inicio s.

--- frontend/src/components/RegisterModal.js
+++ frontend/src/components/RegisterModal.js
@@ -1,71 +1,96 @@
-import React, { useState } from 'react';
-import '../styles/RegisterModal.css';
+import React, { useState} from "react";
+import "../styles/RegisterModal.css";
 
 const RegisterModal = ({ isOpen, onClose }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [carrera, setCarrera] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [carrera, setCarrera] = useState("");
   const [isLogin, setIsLogin] = useState(false);
 
   const carreras = [
-    'Adm. Empresas',
-    'Ing. Comercial',
-    'Ing. Financiera',
-    'Derecho',
-    'Comunicación',
-    'Ing. Civil',
-    'Ing. Industrial',
-    'Ing. Sistemas',
-    'Ing. Mecánica',
-    'Ing. Electrónica',
-    'Ing. Ambiental',
-    'Gastronomía',
-    'Veterinaria'
+    "Adm. Empresas",
+    "Ing. Comercial",
+    "Ing. Financiera",
+    "Derecho",
+    "Comunicación",
+    "Ing. Civil",
+    "Ing. Industrial",
+    "Ing. Sistemas",
+    "Ing. Mecánica",
+    "Ing. Electrónica",
+    "Ing. Ambiental",
+    "Gastronomía",
+    "Veterinaria",
   ];
+ //funcion para limpiar campos
+ const clearInputs = () => {
+  setUsername("");
+  setPassword("");
+  setCarrera("");
+};
+
+
 
   if (!isOpen) return null;
 
   const handleRegister = async () => {
     const response = await fetch(`${window.origin}/register`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ username, password, carrera }),
     });
 
     if (response.ok) {
-      alert('Usuario registrado correctamente');
+      alert("Usuario registrado correctamente");
+      //llimpiado de los campos
+      clearInputs();
       onClose();
     } else {
-      alert('Error al registrar el usuario');
+      alert("Error al registrar el usuario");
+      //limpiado de los campos
+      clearInputs();
     }
   };
 
   const handleLogin = async () => {
     const response = await fetch(`${window.origin}/login`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ username, password }),
     });
 
     if (response.ok) {
       const data = await response.json();
-      localStorage.setItem('token', data.access_token);
-      alert('Logeo exitoso');
+      localStorage.setItem("token", data.access_token);
+      alert("Logeo exitoso");
+      //limpiado de los campos
+      clearInputs();
       onClose();
     } else {
-      alert('El usuario ya existe o la contraseña es incorrecta');
+      alert("El usuario ya existe o la contraseña es incorrecta");
+      //limpiado de los campos
+      clearInputs();
     }
   };
 
   return (
     <div className="register-modal">
       <div className="modal-content">
-        <button className="close-button" onClick={onClose}>×</button>
-        <h2>{isLogin ? 'Iniciar Sesión' : 'Registro'}</h2>
+      <button
+          className="close-button"
+          onClick={() => {
+            onClose();
+            // la función clearInputs aquí para limpiar los campos cuando se cierra el modal
+            clearInputs();
+          }}
+        >
+          ×
+        </button>
+        <h2>{isLogin ? "Iniciar Sesión" : "Registro"}</h2>
         <input
           type="text"
           placeholder="Nombre de Usuario"
@@ -77,17 +102,19 @@ const RegisterModal = ({ isOpen, onClose }) => {
             value={carrera}
             onChange={(e) => setCarrera(e.target.value)}
             style={{
-              width: '100%',
-              padding: '15px',
-              marginBottom: '15px',
-              border: '1px solid #ccc',
-              borderRadius: '4px',
-              fontSize: '16px',
-              backgroundColor: '#f9f9f9',
-              color: '#333',
+              width: "100%",
+              padding: "15px",
+              marginBottom: "15px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+              fontSize: "16px",
+              backgroundColor: "#f9f9f9",
+              color: "#333",
             }}
           >
-            <option value="" disabled>Selecciona tu carrera</option>
+            <option value="" disabled>
+              Selecciona tu carrera
+            </option>
             {carreras.map((carrera, index) => (
               <option key={index} value={carrera}>
                 {carrera}
@@ -97,15 +124,22 @@ const RegisterModal = ({ isOpen, onClose }) => {
         )}
         <input
           type="password"
-          placeholder="Contraseña"
+          placeholder="Codigo Universitario"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
         <button onClick={isLogin ? handleLogin : handleRegister}>
-          {isLogin ? 'Iniciar Sesión' : 'Registrarse'}
+          {isLogin ? "Iniciar Sesión" : "Registrarse"}
         </button>
-        <button onClick={() => setIsLogin(!isLogin)}>
-          {isLogin ? '¿No tienes cuenta? Regístrate' : '¿Ya tienes cuenta? Inicia Sesión'}
+        <button
+          onClick={() => {
+            setIsLogin(!isLogin);
+            clearInputs();
+          }}
+        >
+          {isLogin
+            ? "¿No tienes cuenta? Regístrate"
+            : "¿Ya tienes cuenta? Inicia Sesión"}
         </button>
       </div>
     </div>
