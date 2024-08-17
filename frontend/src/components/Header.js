6adb26ae9074aa9567d71e261f4cdc64e76f0464
marcosrmc3 <mamanimarcos113@gmail.com>
2024-08-17 12:27:50 -0400

    mejorando el header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,14 +2,12 @@ import React from "react";
 import user_icon from "../assets/images/user_icon.png";
 import chat_public_icon from "../assets/images/chat_public_icon.png";
 
-const Header = ({ onMenuClick, onRegisterClick }) => {
+const Header = ({onRegisterClick }) => {
   return (
-    <header className="bg-blue-500 p-4">
+    <header className="bg-gray-800 p-4 w-full fixed">
       <div className="flex items-center justify-between max-w-screen-xl mx-auto">
         <div className="flex items-center">
-          <button className="p-2" onClick={onMenuClick}>
-            <img src={chat_public_icon} alt="Chat icon" className="h-8 w-8" />
-          </button>
+          
         </div>
         <div className="flex-1 text-center">
           <h1 className="text-2xl font-bold text-white">Loyobot</h1>
