9af5299a307cb89ca83861acf23e49f915bc3299
marcosrmc3 <mamanimarcos113@gmail.com>
2024-08-22 12:32:29 -0400

    areglando el register modal directorios y dependencias para que funcione el boton

--- frontend/src/components/RegisterModal.js
+++ frontend/src/components/RegisterModal.js
@@ -1,4 +1,5 @@
 import React, { useState} from "react";
+import "../assets/styles/RegisterModal.css";
 
 const RegisterModal = ({ isOpen, onClose }) => {
   const [username, setUsername] = useState("");
