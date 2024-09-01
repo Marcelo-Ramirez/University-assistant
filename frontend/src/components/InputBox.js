0cf066a1f699b65e47a16bec01a0af27c82da974
Felipe Tuco <felipetucot@gmail.com>
2024-09-01 19:09:19 -0400

    fix(InputBox): prevent sending empty messages

--- frontend/src/components/InputBox.js
+++ frontend/src/components/InputBox.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import BotContext from "../context/BotContext";
 import ChatGlobalContext from "../context/ChatGlobalContext";
 import { useLocation } from "react-router-dom";
@@ -10,6 +9,16 @@ function InputBox({className}) {
     const context = location.pathname == "/bot" ? BotContext : ChatGlobalContext
     const { setInput, input, isSending, handleSend } = useContext(context);
 
+    // Nueva función para manejar el envío que incluye la verificación
+    const handleSendClick = () => {
+        if (input.trim() === "") {
+            // Si el input está vacío, no hacemos nada
+            return;
+        }
+        // si el input esta con informacion pues llamamos a
+        handleSend();
+    };
+
     return (
         <div className= {`${className} col-span-12 row-span-2 w-full flex Sticky items-center border-t-2 border-l-2 border-r-2 border-red-500 rounded-t-lg p-4 overflow-hidden`}>
             <input
@@ -18,7 +27,7 @@ function InputBox({className}) {
                 onChange={(e) => setInput(e.target.value)}
                 onKeyDown={(e) => {
                     if (e.key === "Enter") {
-                        handleSend();
+                        handleSendClick();
                         e.preventDefault();
                     }
                 }}
@@ -27,7 +36,7 @@ function InputBox({className}) {
                 className="flex-1 p-2 border-[1.5px] border-gray-500 rounded focus:outline-none focus:ring focus:border-blue-300"
             />
             <button
-                onClick={() => handleSend()}
+                onClick={handleSendClick}
                 disabled={isSending}
                 className={`text-white px-4 ml-2 py-2 rounded ${isSending ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600'}`}
             >
