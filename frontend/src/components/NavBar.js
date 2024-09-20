b90d4b1e37b9d018cf5b884b6449b8ec1dbe72c0
Felipe Tuco <felipetucot@gmail.com>
2024-09-20 19:27:23 -0400

    Solucionando Bug del navBar que al seleccionar no se quedaba marcado el color azul

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,44 +1,63 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from "react-router-dom";
 import { useDevice } from '../context/DeviceContext';
 import { useInputFocus } from '../context/InputFocusContext';
 
 const NavBar = ({ className }) => {
     const { deviceType } = useDevice();
     const { isInputFocused } = useInputFocus();
+    const location = useLocation(); // Obtiene la ubicación actual
+
+    const [activeLink, setActiveLink] = useState(location.pathname); // Inicializa con la ruta actual
+
+    const handleLinkClick = (link) => {
+        setActiveLink(link); // Cambia el enlace activo al que se hace clic
+    };
+
+    // Establece el enlace activo basado en la ruta actual
+    useEffect(() => {
+        setActiveLink(location.pathname);
+    }, [location.pathname]);
 
     return (
         <nav className={`${className} ${deviceType === 'Mobile' && isInputFocused ? 'hidden' : ''} bg-gray-800 text-white`}>
             <ul className="grid grid-cols-3 gap-4 sm:gap-0">
                 <li className="flex justify-center items-center">
-                    <Link to="/" className="inline-flex flex-col items-center justify-center px-8 py-2 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-                        {/* Lo pongo ahi el link para que el svg y el texto sea un link */}
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="-2 0 24 24" strokeWidth={1.5} stroke="currentColor"
-                            className="h-7 w-7 text-gray-500 dark:text-gray-50 group-hover:text-blue-600 dark:group-hover:text-blue-500">
-                            <path strokeLinecap="round" strokeLinejoin="round"
-                                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                            />
+                    <Link
+                        to="/"
+                        onClick={() => handleLinkClick('/')}
+                        className={`inline-flex flex-col items-center justify-center px-8 py-2 ${activeLink === '/' ? 'text-blue-600' : 'text-gray-300'} hover:bg-gray-50 dark:hover:bg-gray-800 group`}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="-2 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-7 w-7">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                         </svg>
-                        <span class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
+                        <span className={`text-sm ${activeLink === '/' ? 'text-blue-600' : 'text-gray-500'} dark:text-gray-400`}>
                             Inicio
                         </span>
                     </Link>
                 </li>
                 <li className="flex justify-center items-center">
-                    <Link to="/chat" className="inline-flex flex-col items-center justify-center px-8 py-2 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-                        {/* Lo pongo ahi el link para que el svg y el texto sea un link */}
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 text-gray-500 dark:text-gray-50 group-hover:text-blue-600 dark:group-hover:text-blue-500" viewBox="0 0 24 24">
+                    <Link
+                        to="/chat"
+                        onClick={() => handleLinkClick('/chat')}
+                        className={`inline-flex flex-col items-center justify-center px-8 py-2 ${activeLink === '/chat' ? 'text-blue-600' : 'text-gray-300'} hover:bg-gray-50 dark:hover:bg-gray-800 group`}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 24 24">
                             <path fill="currentColor" d="M4 4h16v12H5.17L4 17.17zm0-2c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm2 10h8v2H6zm0-3h12v2H6zm0-3h12v2H6z" />
                         </svg>
-                        <span class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
+                        <span className={`text-sm ${activeLink === '/chat' ? 'text-blue-600' : 'text-gray-500'} dark:text-gray-400`}>
                             Chat
                         </span>
                     </Link>
                 </li>
                 <li className="flex justify-center items-center">
-                    <Link to="/bot" className="inline-flex flex-col items-center justify-center px-8 py-2 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-                        <svg class="w-7 h-7 text-gray-500 dark:text-gray-50 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                            <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.3" color="currentColor">
+                    <Link
+                        to="/bot"
+                        onClick={() => handleLinkClick('/bot')}
+                        className={`inline-flex flex-col items-center justify-center px-8 py-2 ${activeLink === '/bot' ? 'text-blue-600' : 'text-gray-300'} hover:bg-gray-50 dark:hover:bg-gray-800 group`}
+                    >
+                        <svg className="w-7 h-7" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.3">
                                 <path d="M4 15.5a2 2 0 1 1 0-4m16 4a2 2 0 1 0 0-4M7 7V4m10 3V4" />
                                 <circle cx="7" cy="3" r="1" />
                                 <circle cx="17" cy="3" r="1" />
@@ -46,7 +65,7 @@ const NavBar = ({ className }) => {
                                 <path d="M9.5 15c.57.607 1.478 1 2.5 1s1.93-.393 2.5-1m-5.491-4H9m6.009 0H15" />
                             </g>
                         </svg>
-                        <span class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
+                        <span className={`text-sm ${activeLink === '/bot' ? 'text-blue-600' : 'text-gray-500'} dark:text-gray-400`}>
                             Bot
                         </span>
                     </Link>
@@ -54,6 +73,6 @@ const NavBar = ({ className }) => {
             </ul>
         </nav>
     );
-}
+};
 
 export default NavBar;
